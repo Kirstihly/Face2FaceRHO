a3eda1d8a18e75026d8cc985320ae5b310da4c01
Leying Hu <leying.hu@innopeaktech.com>
2023-01-27 19:21:46 -0800

    add video stitch

--- src/fitting.py
+++ src/fitting.py
@@ -8,40 +8,89 @@ from external.decalib.utils.config import cfg as deca_cfg
 from external.decalib.deca import DECA
 from external.decalib.datasets import datasets
 from external.decalib.models.FLAME import FLAME
-from util.util import (
-    save_coeffs,
-    save_landmarks
-)
+from util.util import save_coeffs, save_landmarks
 
 
 def parse_args():
     """Configurations."""
-    parser = argparse.ArgumentParser(description='test process of Face2FaceRHO')
-    parser.add_argument('--device', default='cuda', type=str, help='set device, cpu for using cpu')
-    parser.add_argument('--src_img', type=str, required=True, help='input source image (.jpg, .jpg, .jpeg, .png)')
-    parser.add_argument('--drv_img', type=str, required=True, help='input driving image (.jpg, .jpg, .jpeg, .png)')
+    parser = argparse.ArgumentParser(description="test process of Face2FaceRHO")
+    parser.add_argument(
+        "--device", default="cuda", type=str, help="set device, cpu for using cpu"
+    )
+    parser.add_argument(
+        "--src_img",
+        type=str,
+        required=True,
+        help="input source image (.jpg, .bmp, .jpeg, .png)",
+    )
+    parser.add_argument(
+        "--drv_img",
+        type=str,
+        required=True,
+        help="input driving image (.jpg, .bmp, .jpeg, .png)",
+    )
 
-    parser.add_argument('--output_src_headpose', type=str,
-                        default=os.path.join(os.path.dirname(__file__), '..', 'test_case', 'source', 'FLAME', 'headpose.txt'),
-                        help='output head pose coefficients of source image (.txt)')
-    parser.add_argument('--output_src_landmark', type=str,
-                        default=os.path.join(os.path.dirname(__file__), '..', 'test_case', 'source', 'FLAME', 'landmark.txt'),
-                        help='output facial landmarks of source image (.txt)')
+    parser.add_argument(
+        "--output_src_headpose",
+        type=str,
+        default=os.path.join(
+            os.path.dirname(__file__),
+            "..",
+            "test_case",
+            "source",
+            "FLAME",
+            "headpose.txt",
+        ),
+        help="output head pose coefficients of source image (.txt)",
+    )
+    parser.add_argument(
+        "--output_src_landmark",
+        type=str,
+        default=os.path.join(
+            os.path.dirname(__file__),
+            "..",
+            "test_case",
+            "source",
+            "FLAME",
+            "landmark.txt",
+        ),
+        help="output facial landmarks of source image (.txt)",
+    )
 
-    parser.add_argument('--output_drv_headpose', type=str,
-                        default=os.path.join(os.path.dirname(__file__), '..', 'test_case', 'driving', 'FLAME', 'headpose.txt'),
-                        help=' output head pose coefficients of driving image (.txt)')
-    parser.add_argument('--output_drv_landmark', type=str,
-                        default=os.path.join(os.path.dirname(__file__), '..', 'test_case', 'driving', 'FLAME', 'landmark.txt'),
-                        help='output driving facial landmarks (.txt, reconstructed by using shape coefficients '
-                             'of the source actor and expression and head pose coefficients of the driving actor)')
+    parser.add_argument(
+        "--output_drv_headpose",
+        type=str,
+        default=os.path.join(
+            os.path.dirname(__file__),
+            "..",
+            "test_case",
+            "driving",
+            "FLAME",
+            "headpose.txt",
+        ),
+        help=" output head pose coefficients of driving image (.txt)",
+    )
+    parser.add_argument(
+        "--output_drv_landmark",
+        type=str,
+        default=os.path.join(
+            os.path.dirname(__file__),
+            "..",
+            "test_case",
+            "driving",
+            "FLAME",
+            "landmark.txt",
+        ),
+        help="output driving facial landmarks (.txt, reconstructed by using shape coefficients "
+        "of the source actor and expression and head pose coefficients of the driving actor)",
+    )
 
     return _check_args(parser.parse_args())
 
 
 def _check_args(args):
     if args is None:
-        raise RuntimeError('Invalid arguments!')
+        raise RuntimeError("Invalid arguments!")
     return args
 
 
@@ -50,15 +99,19 @@ class FLAMEFitting:
         self.deca = DECA(config=deca_cfg, device=args.device)
 
     def fitting(self, img_name):
-        testdata = datasets.TestData(img_name, iscrop=True,face_detector='fan', sample_step=10)
+        testdata = datasets.TestData(
+            img_name, iscrop=True, face_detector="fan", sample_step=10
+        )
         input_data = testdata[0]
-        images = input_data['image'].to(args.device)[None, ...]
+        images = input_data["image"].to(args.device)[None, ...]
         with torch.no_grad():
             codedict = self.deca.encode(images)
-            codedict['tform'] = input_data['tform'][None, ...]
-            original_image = input_data['original_image'][None, ...]
+            codedict["tform"] = input_data["tform"][None, ...]
+            original_image = input_data["original_image"][None, ...]
             _, _, h, w = original_image.shape
-            params = self.deca.ensemble_3DMM_params(codedict, image_size=deca_cfg.dataset.image_size, original_image_size=h)
+            params = self.deca.ensemble_3DMM_params(
+                codedict, image_size=deca_cfg.dataset.image_size, original_image_size=h
+            )
         return params
 
 
@@ -66,14 +119,16 @@ class PoseLandmarkExtractor:
     def __init__(self):
         self.flame = FLAME(deca_cfg.model)
 
-        with open(os.path.join(deca_cfg.deca_dir, 'data', 'pose_transform_config.json'), 'r') as f:
+        with open(
+            os.path.join(deca_cfg.deca_dir, "data", "pose_transform_config.json"), "r"
+        ) as f:
             pose_transform = json.load(f)
 
-        self.scale_transform = pose_transform['scale_transform']
-        self.tx_transform = pose_transform['tx_transform']
-        self.ty_transform = pose_transform['ty_transform']
-        self.tx_scale = 0.256 # 512 / 2000
-        self.ty_scale = - self.tx_scale
+        self.scale_transform = pose_transform["scale_transform"]
+        self.tx_transform = pose_transform["tx_transform"]
+        self.ty_transform = pose_transform["ty_transform"]
+        self.tx_scale = 0.256  # 512 / 2000
+        self.ty_scale = -self.tx_scale
 
     @staticmethod
     def transform_points(points, scale, tx, ty):
@@ -86,7 +141,9 @@ class PoseLandmarkExtractor:
         trans_matrix[:, 3, 3] = 1
 
         batch_size, n_points, _ = points.shape
-        points_homo = torch.cat([points, torch.ones([batch_size, n_points, 1], dtype=points.dtype)], dim=2)
+        points_homo = torch.cat(
+            [points, torch.ones([batch_size, n_points, 1], dtype=points.dtype)], dim=2
+        )
         points_homo = points_homo.transpose(1, 2)
         trans_points = torch.bmm(trans_matrix, points_homo).transpose(1, 2)
         trans_points = trans_points[:, :, 0:3]
@@ -97,35 +154,45 @@ class PoseLandmarkExtractor:
         expression_params = torch.tensor(expression_params).unsqueeze(0)
         pose = torch.tensor(pose).unsqueeze(0)
         verts, landmarks3d = self.flame(
-            shape_params=shape_params, expression_params=expression_params, pose_params=pose)
+            shape_params=shape_params,
+            expression_params=expression_params,
+            pose_params=pose,
+        )
         trans_landmarks3d = self.transform_points(landmarks3d, scale, tx, ty)
         trans_landmarks3d = trans_landmarks3d.squeeze(0).cpu().numpy()
         return trans_landmarks3d[:, 0:2].tolist()
 
-    def calculate_nose_tip_tx_ty(self, shape_params, expression_params, pose, scale, tx, ty):
+    def calculate_nose_tip_tx_ty(
+        self, shape_params, expression_params, pose, scale, tx, ty
+    ):
         front_pose = copy.deepcopy(pose)
         front_pose[0] = front_pose[1] = front_pose[2] = 0
-        front_landmarks3d = self.get_project_points(shape_params, expression_params, front_pose, scale, tx, ty)
-        original_landmark3d = self.get_project_points(shape_params, expression_params, pose, scale, tx, ty)
+        front_landmarks3d = self.get_project_points(
+            shape_params, expression_params, front_pose, scale, tx, ty
+        )
+        original_landmark3d = self.get_project_points(
+            shape_params, expression_params, pose, scale, tx, ty
+        )
         nose_tx = original_landmark3d[30][0] - front_landmarks3d[30][0]
         nose_ty = original_landmark3d[30][1] - front_landmarks3d[30][1]
         return nose_tx, nose_ty
 
     def get_pose(self, shape_params, expression_params, pose, scale, tx, ty):
         nose_tx, nose_ty = self.calculate_nose_tip_tx_ty(
-            shape_params, expression_params, pose, scale, tx, ty)
-        transformed_axis_angle = [
-            float(pose[0]),
-            float(pose[1]),
-            float(pose[2])
-        ]
+            shape_params, expression_params, pose, scale, tx, ty
+        )
+        transformed_axis_angle = [float(pose[0]), float(pose[1]), float(pose[2])]
         transformed_tx = self.tx_transform + self.tx_scale * (tx + nose_tx)
         transformed_ty = self.ty_transform + self.ty_scale * (ty + nose_ty)
         transformed_scale = scale / self.scale_transform
-        return transformed_axis_angle + [transformed_tx, transformed_ty, transformed_scale]
+        return transformed_axis_angle + [
+            transformed_tx,
+            transformed_ty,
+            transformed_scale,
+        ]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
 
     # 3DMM fitting by DECA: Detailed Expression Capture and Animation using FLAME model
@@ -136,22 +203,42 @@ if __name__ == '__main__':
     # calculate head pose and facial landmarks for the source and driving face images
     pose_lml_extractor = PoseLandmarkExtractor()
     src_headpose = pose_lml_extractor.get_pose(
-        src_params['shape'], src_params['exp'], src_params['pose'],
-        src_params['scale'], src_params['tx'], src_params['ty'])
+        src_params["shape"],
+        src_params["exp"],
+        src_params["pose"],
+        src_params["scale"],
+        src_params["tx"],
+        src_params["ty"],
+    )
 
     src_lmks = pose_lml_extractor.get_project_points(
-        src_params['shape'], src_params['exp'], src_params['pose'],
-        src_params['scale'], src_params['tx'], src_params['ty'])
+        src_params["shape"],
+        src_params["exp"],
+        src_params["pose"],
+        src_params["scale"],
+        src_params["tx"],
+        src_params["ty"],
+    )
 
     # Note that the driving head pose and facial landmarks are calculated using the shape parameters of the source image
     # in order to eliminate the interference of the driving actor's identity.
     drv_headpose = pose_lml_extractor.get_pose(
-        src_params['shape'], drv_params['exp'], drv_params['pose'],
-        drv_params['scale'], drv_params['tx'], drv_params['ty'])
+        src_params["shape"],
+        drv_params["exp"],
+        drv_params["pose"],
+        drv_params["scale"],
+        drv_params["tx"],
+        drv_params["ty"],
+    )
 
     drv_lmks = pose_lml_extractor.get_project_points(
-        src_params['shape'], drv_params['exp'], drv_params['pose'],
-        drv_params['scale'], drv_params['tx'], drv_params['ty'])
+        src_params["shape"],
+        drv_params["exp"],
+        drv_params["pose"],
+        drv_params["scale"],
+        drv_params["tx"],
+        drv_params["ty"],
+    )
 
     # save
     os.makedirs(os.path.split(args.output_src_headpose)[0], exist_ok=True)
---
+++ video_demo.py
@@ -0,0 +1,196 @@
+import cv2
+import os
+import shutil
+import subprocess
+
+import numpy as np
+
+from argparse import ArgumentParser
+
+TRIALS = 5
+
+
+def generate_frames(video_file):
+    assert os.path.isfile(video_file), video_file + " not exist!"
+
+    video_dir = video_file.split(".")[0]
+    if os.path.exists(video_dir):
+        shutil.rmtree(video_dir)
+
+    print("Generating frames:", video_dir)
+    os.makedirs(video_dir)
+    command = "ffmpeg -i " + video_file + " -vf fps=30 " + video_dir + "/%05d.png"
+    print(command)
+    subprocess.check_call(command, shell=True, timeout=60)
+
+    frame_num = len(os.listdir(video_dir))
+    print("Frames extracted:", frame_num)
+    return frame_num
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser(
+        description="A Python script to concat source, driving, and generated result videos. \
+                     Assume source and driving videos are saved in data/",
+        epilog="python video_demo.py -s 00001 -d 00051",
+    )
+    parser.add_argument(
+        "-s",
+        "--source",
+        type=str,
+        help="The source video filename.",
+    )
+    parser.add_argument(
+        "-d",
+        "--driving",
+        type=str,
+        help="The driving video filename.",
+    )
+    parser.add_argument(
+        "--format",
+        type=str,
+        default="mp4",
+        help="The video format. Default: mp4.",
+    )
+    args = parser.parse_args()
+
+    source_file = os.path.join("data", args.source + "." + args.format)
+    source_num = generate_frames(source_file)
+
+    driving_file = os.path.join("data", args.driving + "." + args.format)
+    driving_num = generate_frames(driving_file)
+
+    # Process frames
+    for i in range(min(source_num, driving_num)):
+        num_padding = str(i + 1).zfill(5)
+        frame_name = num_padding + ".png"
+        headpose_name = num_padding + "_headpose.txt"
+        landmark_name = num_padding + "_landmark.txt"
+
+        src_headpose = os.path.join("data", "source", "FLAME", headpose_name)
+        src_landmark = os.path.join("data", "source", "FLAME", landmark_name)
+        drv_headpose = os.path.join("data", "driving", "FLAME", headpose_name)
+        drv_landmark = os.path.join("data", "driving", "FLAME", landmark_name)
+        result_name = os.path.join("data", "result", frame_name)
+
+        # Generate facial landmarks and head pose coefficients
+        command = (
+            "python src/fitting.py"
+            + " --device cuda"
+            + " --src_img ./data/"
+            + args.source
+            + "/"
+            + frame_name
+            + " --drv_img ./data/"
+            + args.driving
+            + "/"
+            + frame_name
+            + " --output_src_headpose "
+            + src_headpose
+            + " --output_src_landmark "
+            + src_landmark
+            + " --output_drv_headpose "
+            + drv_headpose
+            + " --output_drv_landmark "
+            + drv_landmark
+        )
+        print(command)
+        trial = 0
+        while trial < TRIALS:
+            trial += 1
+            if not (
+                os.path.exists(src_headpose)
+                and os.path.exists(src_landmark)
+                and os.path.exists(drv_headpose)
+                and os.path.exists(drv_landmark)
+            ):
+                subprocess.check_call(command, shell=True, timeout=60)
+
+        # Generate final reenacted results
+        command = (
+            "python src/reenact.py"
+            + " --config src/config/test_face2facerho.ini"
+            + " --src_img ./data/"
+            + args.source
+            + "/"
+            + frame_name
+            + " --src_headpose "
+            + src_headpose
+            + " --src_landmark "
+            + src_landmark
+            + " --drv_headpose "
+            + drv_headpose
+            + " --drv_landmark "
+            + drv_landmark
+            + " --output_dir ./data/result"
+        )
+        print(command)
+        trial = 0
+        while trial < TRIALS:
+            trial += 1
+            if os.path.exists("./data/result/result.png"):
+                command = "mv ./data/result/result.png " + result_name
+                subprocess.check_call(command, shell=True, timeout=60)
+            elif not os.path.exists(result_name):
+                subprocess.check_call(command, shell=True, timeout=60)
+
+    # Concat source, driving, and generated result videos
+    images = []
+    for i in range(min(source_num, driving_num)):
+        frame_name = str(i + 1).zfill(5) + ".png"
+        source_frame = os.path.join("data", args.source, frame_name)
+        driving_frame = os.path.join("data", args.driving, frame_name)
+        result_frame = os.path.join("data", "result", frame_name)
+
+        source_img = cv2.imread(source_frame, cv2.IMREAD_COLOR)
+        driving_img = cv2.imread(driving_frame, cv2.IMREAD_COLOR)
+        result_img = cv2.imread(result_frame, cv2.IMREAD_COLOR)
+        assert result_img.shape == driving_img.shape
+        assert result_img.shape == source_img.shape
+
+        source_img = cv2.putText(
+            source_img,
+            "SOURCE",
+            (50, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 255, 255),
+            2,
+            cv2.LINE_AA,
+        )
+        driving_img = cv2.putText(
+            driving_img,
+            "DRIVING",
+            (50, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 255, 255),
+            2,
+            cv2.LINE_AA,
+        )
+        result_img = cv2.putText(
+            result_img,
+            "RESULT",
+            (50, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 255, 255),
+            2,
+            cv2.LINE_AA,
+        )
+
+        canvas = np.concatenate((source_img, driving_img), axis=1)
+        canvas = np.concatenate((canvas, result_img), axis=1)
+        images.append(canvas)
+
+    frame_height, frame_width, _ = images[0].shape
+    out = cv2.VideoWriter(
+        os.path.join("data", "result", args.source + ".mp4"),
+        cv2.VideoWriter_fourcc(*"MP4V"),
+        10,
+        (frame_width, frame_height),
+    )
+    for image in images:
+        out.write(image)
+    out.release()
